489a765a3c1541d884d64608db527b960b97623a
Aaron Christiansen <aaronc20000@gmail.com>
2024-04-13 18:27:19 +0100

    Add tokenizer for pattern language

---
+++ lib/pattern_tokenizer.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Token:
+    position: range
+
+@dataclass
+class DatumToken(Token):
+    contents: str
+
+@dataclass
+class QuotedStringToken(Token):
+    contents: str
+
+class SemicolonToken(Token): pass
+class EqualsToken(Token): pass
+
+class UnexpectedEndError(Exception): pass
+
+@dataclass
+class UnexpectedCharacterError(Exception):
+    char: str
+    position: int
+
+class UnterminatedQuotedStringError(Exception): pass
+
+
+class Tokenizer:
+    def __init__(self, input: str):
+        self.input = input
+        self.current_position = 0
+
+    def tokenize(self) -> List[Token]:
+        tokens = []
+
+        while not self.is_at_end():
+            # Skip whitespace
+            self.skip_whitespace()
+            if self.is_at_end(): break
+
+            start_pos = self.current_position
+
+            if self.take_if(";"):
+                tokens.append(SemicolonToken(position=self.range_to_here_from(start_pos)))
+            elif self.take_if("="):
+                tokens.append(EqualsToken(position=self.range_to_here_from(start_pos)))
+
+            elif self.here().isalnum():
+                buffer = ""
+                while not self.is_at_end() and self.here().isalnum():
+                    buffer += self.take()
+                tokens.append(DatumToken(contents=buffer, position=self.range_to_here_from(start_pos)))
+
+            elif self.take_if("\""):
+                buffer = ""
+                while not self.is_at_end() and self.here() != "\"":
+                    buffer += self.take()
+
+                # Consume closing quote
+                if self.is_at_end():
+                    raise UnterminatedQuotedStringError()
+                self.take() 
+
+                tokens.append(QuotedStringToken(contents=buffer, position=self.range_to_here_from(start_pos)))
+
+            else:
+                raise UnexpectedCharacterError(char=self.here(), position=self.current_position)
+            
+        return tokens
+
+    def skip_whitespace(self):
+        while not self.is_at_end() and self.here().isspace():
+            self.take()
+
+    def is_at_end(self) -> bool:
+        return self.current_position >= len(self.input)
+
+    def take(self) -> str:
+        """Consume one character from the input, and return it."""
+
+        char = self.here()
+        self.current_position += 1
+        return char
+    
+    def take_if(self, expected: str) -> bool:
+        """If the next character from the input matches a given one, consume it and return True."""
+
+        char = self.here()
+        if char == expected:
+            self.take()
+            return True
+        return False
+
+    def here(self) -> str:
+        """Return the current character from the input, without changing it."""
+
+        if self.is_at_end():
+            raise UnexpectedEndError()
+    
+        return self.input[self.current_position]
+
+    def range_to_here_from(self, start_pos) -> range:
+        return range(start_pos, self.current_position)
---
+++ test/test_pattern_tokenizer.py
@@ -0,0 +1,26 @@
+import pytest
+from ..lib.pattern_tokenizer import *
+
+def test_tokenize_data():
+    assert Tokenizer("ab 12 34 5").tokenize() == [
+        DatumToken(contents="ab", position=range(0, 2)),
+        DatumToken(contents="12", position=range(3, 5)),
+        DatumToken(contents="34", position=range(6, 8)),
+        DatumToken(contents="5",  position=range(9, 10)),
+    ]
+
+def test_tokenize_symbols():
+    assert Tokenizer("\"ABC\" = ab ;").tokenize() == [
+        QuotedStringToken(contents="ABC", position=range(0, 5)),
+        EqualsToken(                      position=range(6, 7)),
+        DatumToken(contents="ab",         position=range(8, 10)),
+        SemicolonToken(                   position=range(11, 12)),
+    ]
+
+def test_unknown():
+    with pytest.raises(UnexpectedCharacterError):
+        Tokenizer("?").tokenize()
+
+def test_unterminated_string():
+    with pytest.raises(UnterminatedQuotedStringError):
+        Tokenizer("\"ABC = ab ;").tokenize()
